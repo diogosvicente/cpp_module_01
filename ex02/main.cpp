faee906e3a71e71da0881a4e1672e108a320197a
Diogo Vicente <diogosk84ever@hotmail.com>
2023-05-22 17:26:22 +0000

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,6 +2,18 @@
 
 int	main(void)
 {
-	std::cout << "Hello World!" << std::endl;
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << std::endl << "---------- Printing Memory Addresses ----------" << std::endl;
+	std::cout << "The memory address of the string variable: \t" << &string << std::endl;
+	std::cout << "The memory address held by stringPTR: \t\t" << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: \t\t" << &stringREF << std::endl;
+
+	std::cout << std::endl << "---------- Printing String Values ----------" << std::endl;
+	std::cout << "The value of the string variable: \t" << string << std::endl;
+	std::cout << "The value pointed to by stringPTR: \t" << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: \t" << stringREF << std::endl << std::endl;
 	return (0);
 }
