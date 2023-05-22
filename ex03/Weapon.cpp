29130358cfb437ca7236033dc4bfcd10b620e52b
Diogo Vicente <diogosk84ever@hotmail.com>
2023-05-22 18:49:58 +0000

    ex03 begg

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,11 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon()
+{
+	std::cout << "Constructing" << std::endl;
+}
+
+Weapon::~Weapon()
+{
+	std::cout << "Destructing" << std::endl;
+}
---
+++ ex03/main.cpp
@@ -0,0 +1,7 @@
+#include "Weapon.hpp"
+
+int	main(void)
+{
+	std::cout << "Teste\n";
+	return (0);
+}
